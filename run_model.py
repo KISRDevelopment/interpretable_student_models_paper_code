75e4bba3b83a9710bab9d1bd7f71ebceb856f059
Mohammad Khajah <mmkhajah@gmail.com>
2022-09-06 10:51:47 +0000

    update run model

--- run_model.py
+++ run_model.py
@@ -11,13 +11,17 @@ datasets = [os.path.basename(p).replace('.csv','') for p in glob.glob("data/data
 # model_script = "torch_bkt_problems.py"
 # cfg_name = "bkt-problems"
 
-cfg_path = "cfgs/dkt-pytorch.json"
-model_script = "dkt.py"
-cfg_name = "dkt"
+# cfg_path = "cfgs/dkt-pytorch.json"
+# model_script = "dkt.py"
+# cfg_name = "dkt"
 
 # cfg_path = "cfgs/bkt-abilities-pytorch.json"
 # model_script = "torch_bkt_irt.py"
 # cfg_name = "bkt-irt"
+
+cfg_path = "cfgs/bkt-irt-lr.json"
+model_script = "torch_bkt_irt_lr.py"
+cfg_name = "bkt-lr"
 
 for dataset in datasets:
     if os.path.exists("data/results-pytorch/%s_%s.csv"%(cfg_name, dataset)):
