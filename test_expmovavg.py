96f6a291faf965a4b0f7e71a15397e0bb41c19b4
Mohammad Khajah <mmkhajah@gmail.com>
2023-05-01 11:28:01 +0000

    add batching to expmovavg

--- test_expmovavg.py
+++ test_expmovavg.py
@@ -2,60 +2,68 @@ import numpy as np
 
 def main():
     np.random.seed(53345)
-    alpha = 0.63
-    beta = 0.3
-    h1 = 0.1
+    alpha = 0.4
+    beta = 0.1
+    h1 = 0.35
 
-    y = np.array([1, 0, 0, 1.0])
-    
+    y = np.array([[1, 0, 0, 1.0], [0, 1, 0, 1.0]])
     y_pred = np.zeros_like(y).astype(float)
-    y_pred[0] = h1
 
-    h = h1
-    for i in range(1, y_pred.shape[0]):
-        h = y[i-1] * alpha + (y[i-1] * (1 - alpha - beta) + beta) * h
-        y_pred[i] = h 
+    for s in range(y.shape[0]):
+        y_pred[s, 0] = h1
+
+        h = h1
+        for i in range(1, y_pred.shape[1]):
+            h = y[s, i-1] * alpha + (y[s, i-1] * (1 - alpha - beta) + beta) * h
+            y_pred[s, i] = h 
     
-    print("Reference:")
-    print(y)
-    print(y_pred)
-
-    print("Non-recursive:")
-
+    
+    #
+    # non-recursive
+    #
+    seqs = y
+    
     #
     # create matrix of Fs (one row for each time step) TxT
     #
-    fs = y * (1- alpha - beta) + beta 
-    t = y.shape[0]
-    Fs = np.tril(np.ones((t, t)), -1) * fs[None,:] + np.triu(np.ones((t, t)), 0)
-    print("Fs:")
-    print(Fs)
-
+    fs = seqs * (1- alpha - beta) + beta  # BxT
+    t = seqs.shape[1]
+    Fs = np.tril(np.ones((t, t)), -1) * fs[:,None,:] + np.triu(np.ones((t, t)), 0)
+    print("Log Fs:")
+    logFs = np.log(Fs)
+    print(logFs) # BxTxT
+    
     #
     # Compute reverse cumulative product row-wise
     #
     rev_idx = np.arange(t-1, -1, -1)
-    rev_cumprod = np.cumprod(Fs[:, rev_idx], axis=1)[:,rev_idx]
-    print("Reverse cumulative product:")
-    print(rev_cumprod)
-
+    rev_cumsum = np.cumsum(logFs[:, :, rev_idx], axis=2)[:, :,rev_idx]
+    print("Reverse cumulative sum:")
+    print(rev_cumsum) # BxTxT
+    
     #
     # Multiply by alphas and observations
     #
     alphas = np.ones(t) * alpha 
     alphas[0] = 1
-    xs = np.roll(y, 1)
-    xs[0] = h1 
+    xs = np.roll(seqs, 1, axis=1) # BxT
+    xs[:,0] = h1 
     print("Xs:")
     print(xs)
-
-    print("Mask:")
+    
     mask = np.tril(np.ones(t), 0)
-    print(mask)
-
-    R = rev_cumprod * alphas[None,:] * xs[None,:] * mask
-    print("R:")
+    
+    R = np.exp(rev_cumsum + np.log(alphas[None,None,:])) * xs[:,None,:] * mask[None,:,:]
     print(R)
-    print(np.sum(R, axis=1))
+
+    print("Seq:")
+    print(y)
+    
+    print("Reference:")
+    print(y_pred)
+
+    print("New:")
+    print(np.sum(R, axis=2))
+
 if __name__ == "__main__":
     main()
