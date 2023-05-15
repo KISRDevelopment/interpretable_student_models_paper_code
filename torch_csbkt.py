4b7ad1072ac4aed8f009ea3b274b8f3a6f1a012a
Mohammad Khajah <mmkhajah@gmail.com>
2023-05-15 11:33:54 +0000

    add nido module to csbkt

--- torch_csbkt.py
+++ torch_csbkt.py
@@ -1,8 +1,11 @@
-#import tensorflow as tf
+
 import numpy as np 
 import itertools 
 import torch as th 
 import torch.nn.functional as F 
+from torch import Tensor
+from typing import List, Tuple
+import torch.nn as nn 
 
 def main():
     n_skills = 3
@@ -26,6 +29,33 @@ def main():
     initial_log_prob_vec = make_initial_log_prob_vector(decoder, logit_pi)
     print(initial_log_prob_vec.exp().sum())
     
+    model = HmmCell(32, 2)
+    n_batch = 4 
+    timesteps = 7 
+
+    trans_log_probs = A # [n_states, n_states]
+    init_log_probs = th.tile(initial_log_prob_vec, (1, n_batch)).T # [n_batch, n_states]
+    
+    obs_probs = th.softmax(th.randn(2**n_skills, 2), axis=1) # [n_states, 2]
+    obs_probs = th.tile(obs_probs[None, :, :], (timesteps, 1, 1))
+    obs_probs = th.tile(obs_probs[None, :, :, :], (n_batch, 1, 1, 1))
+    obs_log_probs = obs_probs.log()
+    
+    obs = np.random.binomial(1, 0.5, (n_batch, timesteps))
+    obs = th.tensor(obs).long()
+
+    n_problems = 20
+    nido_layer = NIDOLayer(n_problems, decoder)
+
+    problem_seq = np.tile(np.random.permutation(n_problems)[:timesteps][None,:], (n_batch, 1))
+    problem_seq = th.tensor(problem_seq).long()
+
+    obs_log_probs = nido_layer(problem_seq)
+    result,state = model(obs, trans_log_probs, obs_log_probs, init_log_probs)
+    print(result.exp())
+    print(state.exp())
+
+
 def make_state_decoder_matrix(k):
     """
         creates a lookup table that maps integer states into
@@ -121,6 +151,96 @@ def make_transition_log_prob_matrix(im, logit_pL, logit_pF):
     # finally, compute summation to get the log transition matrix
     return U.sum(2) # [n_total, n_total]
 
+class HmmCell(th.jit.ScriptModule):
+    
+    def __init__(self, n_states, n_outputs):
+        super(HmmCell, self).__init__()
+        
+        self.n_states = n_states
+        self.n_outputs = n_outputs
+        
+    @th.jit.script_method
+    def forward(self, obs: Tensor,
+                      trans_log_probs: Tensor, 
+                      obs_log_probs: Tensor, 
+                      init_log_probs: Tensor) -> Tuple[Tensor, Tensor]:
+        """
+            input:
+                obs: [n_batch, t]
+                trans_log_probs: [n_states, n_states] (Target, Source)
+                obs_log_probs:   [n_batch, t, n_states, n_outputs]
+                init_log_probs:  [n_batch, n_states]
+            output:
+                log_output_prob: [n_batch, t, n_outputs]
+                log_alpha: [n_batch, n_states]
+        """
+        outputs = th.jit.annotate(List[Tensor], [])
+        batch_idx = th.arange(init_log_probs.shape[0])
 
+        # [n_batch, n_states]
+        log_alpha = init_log_probs
+        for i in range(0, obs.shape[1]):
+            
+            # predict
+            # B X S X O + B X S X 1
+            log_py = th.logsumexp(obs_log_probs[:,i,:,:] + log_alpha[:, :, None], dim=1)  #[n_batch, n_obs]
+            log_py = log_py - th.logsumexp(log_py, dim=1)[:,None]
+            outputs += [log_py]
+
+            # update
+            curr_y = obs[:,i]
+            log_py = obs_log_probs[batch_idx, i, :, curr_y] # [n_batch, n_states]
+            
+            # B x 1 X S + B x 1 x S + 1 x S x S
+            log_alpha = th.logsumexp(log_py[:,None,:] + log_alpha[:,None,:] + trans_log_probs[None,:,:], dim=2)
+        
+        outputs = th.stack(outputs)
+        outputs = th.transpose(outputs, 0, 1)
+        
+        return outputs, log_alpha
+
+class NIDOLayer(nn.Module):
+
+    def __init__(self, n_problems, decoder):
+        super().__init__()
+
+        n_states, n_skills = decoder.shape 
+
+        self.skill_offset = nn.Parameter(th.randn(n_skills))
+        self.skill_slope = nn.Parameter(th.randn(n_skills))
+        self.membership_logits = nn.Parameter(th.randn(n_problems, n_skills))
+        
+        self.decoder = decoder # [n_states, n_skills]
+
+    def forward(self, problem_seq):
+        """
+            Input:
+                problem_seq: [n_batch, t]
+            Output:
+                obs_log_probs: [n_batch, t, n_states, 2]
+        """
+
+        #
+        # compute state logits
+        # 1xK + (1xK) * (SxK) = SxK 
+        # [n_states, n_skills]
+        #
+        state_logits = self.skill_offset[None,:] + self.skill_slope[None,:] * self.decoder
+
+        #
+        # compute problem logits
+        #
+        membership_probs = th.sigmoid(self.membership_logits) # [n_problems, n_skills]
+        problem_logits = membership_probs @ state_logits.T # [n_problems, n_states]
+
+        obs_correct_logits = problem_logits[problem_seq] # [n_batch, t, n_states]
+
+        # [n_batch, t, n_states, 2]
+        obs_log_probs = F.log_softmax(th.concat((-obs_correct_logits[:,:,:,None], obs_correct_logits[:,:,:,None]), dim=3), dim=3)
+
+        return obs_log_probs
+
+
+        
 if __name__ == "__main__":
     main()
