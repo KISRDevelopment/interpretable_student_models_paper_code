fix parameter calculation in fbkt

fast_bkt.py
@@ -22,7 +22,7 @@ def main():
     probs = sigmoid(np.array([logit_pL, logit_pF, logit_pG, logit_pS, logit_pI0]))
     
     # test sequence
-    seqs = generate_seqs(100, 500, probs)
+    seqs = generate_seqs(500, 50, probs)
     
     # limit on time steps
     N = 10

model_fbkt.py
@@ -245,19 +245,11 @@ class BktModel(nn.Module):
         logits = self._logits(kc) # Bx5
         return self._model(corr, logits)
 
+    
     def get_params(self):
         
-        kc_logits = self._logits.weight
-        trans_logits = th.hstack((-kc_logits[:, [0]], # 1-pL
-                                kc_logits[:, [1]],  # pF
-                                kc_logits[:, [0]],  # pL
-                                -kc_logits[:, [1]])).reshape((-1, 2, 2)) # 1-pF (Latent KCs x 2 x 2)
-        obs_logits = th.hstack((-kc_logits[:, [2]], # 1-pG
-                                kc_logits[:, [2]],  # pG
-                                kc_logits[:, [3]],  # pS
-                                -kc_logits[:, [3]])).reshape((-1, 2, 2)) # 1-pS (Latent KCs x 2 x 2)
-        init_logits = th.hstack((-kc_logits[:, [4]], kc_logits[:, [4]])) # (Latent KCs x 2)
-
+        
+        trans_logits, obs_logits, init_logits = _get_logits(self._logits.weight)
 
         alpha = F.softmax(init_logits, dim=1) # n_chains x n_states
         obs = F.softmax(obs_logits, dim=2) # n_chains x n_states x n_obs
@@ -265,6 +257,21 @@ class BktModel(nn.Module):
         
         return alpha, obs, t
     
+def _get_logits(kc_logits):
+    
+    trans_logits = th.hstack((  kc_logits[:, [0]]*0, # 1-pL
+                                kc_logits[:, [1]],  # pF
+                                kc_logits[:, [0]],  # pL
+                                kc_logits[:, [1]]*0)).reshape((-1, 2, 2)) # 1-pF (Latent KCs x 2 x 2)
+    obs_logits = th.hstack((kc_logits[:, [2]]*0, # 1-pG
+                            kc_logits[:, [2]],  # pG
+                            kc_logits[:, [3]],  # pS
+                            kc_logits[:, [3]]*0)).reshape((-1, 2, 2)) # 1-pS (Latent KCs x 2 x 2)
+    init_logits = th.hstack((kc_logits[:, [4]]*0, 
+                             kc_logits[:, [4]])) # (Latent KCs x 2)
+
+    return trans_logits, obs_logits, init_logits
+
 class FastBkt(jit.ScriptModule):
 
     def __init__(self, n, device):

results_param_recovery.py
@@ -76,7 +76,7 @@ def get_torch_bkt_probs(params_path, actual_params):
         probs = np.zeros(5)
         for f in range(n_folds):
             diff = np.abs(alpha[f, k, :] - actual_params[k, 0])
-            knowing_state = np.argmin(diff)
+            knowing_state = np.argmin(diff) 
             probs[0] = alpha[f, k, knowing_state]
 
             probs[1] = t[f, k, knowing_state, 1-knowing_state]

torch_bkt.py
@@ -21,16 +21,23 @@ import sklearn.metrics
 
 class MultiHmmCell(jit.ScriptModule):
     
-    def __init__(self, n_states, n_outputs, n_chains):
+    def __init__(self, n_states, n_outputs, n_chains, trans_logits, obs_logits, init_logits):
         super(MultiHmmCell, self).__init__()
         
         self.n_states = n_states
         self.n_outputs = n_outputs
         
+        if trans_logits is None:
+            trans_logits = th.randn(n_chains, n_states, n_states)
+        if obs_logits is None:
+            obs_logits = th.randn(n_chains, n_states, n_outputs)
+        if init_logits is None:
+            init_logits = th.randn(n_chains, n_states)
+
         # [n_hidden,n_hidden] (Target,Source)
-        self.trans_logits = nn.Parameter(th.randn(n_chains, n_states, n_states))
-        self.obs_logits = nn.Parameter(th.randn(n_chains, n_states, n_outputs))
-        self.init_logits = nn.Parameter(th.randn(n_chains, n_states))
+        self.trans_logits = nn.Parameter(trans_logits)
+        self.obs_logits = nn.Parameter(obs_logits)
+        self.init_logits = nn.Parameter(init_logits)
         
     @jit.script_method
     def forward(self, obs: Tensor, chain: Tensor) -> Tensor:
@@ -73,9 +80,9 @@ class MultiHmmCell(jit.ScriptModule):
         return outputs
 
 class BktModel(nn.Module):
-    def __init__(self, n_kcs):
+    def __init__(self, n_kcs, trans_logits=None, obs_logits=None, init_logits=None):
         super(BktModel, self).__init__()
-        self.hmm = MultiHmmCell(2, 2, n_kcs)
+        self.hmm = MultiHmmCell(2, 2, n_kcs, trans_logits, obs_logits, init_logits)
 
     def forward(self, corr, kc):
         return self.hmm(corr, kc)