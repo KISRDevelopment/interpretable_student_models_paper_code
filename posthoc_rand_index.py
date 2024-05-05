26d742a285f02c0d9c07644cb53a40bd26382013
Mohammad Khajah <mmkhajah@gmail.com>
2024-05-05 11:43:51 +0000

    add raw rand index to existing results

---
+++ posthoc_rand_index.py
@@ -0,0 +1,79 @@
+import numpy as np 
+import pandas as pd 
+import sys 
+import sklearn.metrics
+import sklearn.cluster
+import re 
+def main():
+    raw_results_dir = sys.argv[1]
+    results_file = sys.argv[2]
+
+    all_results_df = pd.read_csv(results_file)
+    
+    datasets = set(all_results_df['dataset'])
+    models = set(all_results_df['model'])
+    ri_col = np.zeros(all_results_df.shape[0])
+    for dataset_name in datasets:
+        
+        df = pd.read_csv("data/datasets/%s.csv" % dataset_name)
+        
+        ref_assignment = get_problem_skill_assignment(df)
+        problem_feature_mat = np.load("data/datasets/%s.embeddings.npy" % dataset_name.replace('_blocked', '').replace('_interleaved',''))
+        
+        for model in models:
+            if model.startswith('sd'):
+                if 'realistic' in dataset_name:
+
+                    params_path = "%s/%s_%s.params.npy.npz" % (raw_results_dir, re.sub(r'^sd', 'sd-50kcs', model), dataset_name)
+                else:
+                    params_path = "%s/%s_%s.params.npy.npz" % (raw_results_dir, model, dataset_name)
+                params = np.load(params_path)
+                Aprior = params['Aprior'] # Splits x Problems x KCs
+            
+                ri = []
+                for i in range(Aprior.shape[0]):
+                    Q = Aprior[i, :, :] 
+                    pred_assignment = np.argmax(Q, axis=1)
+                    rand_index = sklearn.metrics.rand_score(ref_assignment, pred_assignment)
+                    ri.append(rand_index)
+
+                
+            elif model.startswith('clustering') and not dataset_name.startswith('sd_1_'):
+                splits = np.load("data/splits/%s.npy" % dataset_name)
+                
+                ri = []
+                for s in range(splits.shape[0]):
+                    split = splits[s, :]
+
+                    train_ix = split == 2
+                    train_df = df[train_ix]
+
+                    #
+                    # build problem clustering model based on training problems only
+                    #
+                    train_problems = sorted(pd.unique(train_df['problem']))
+                    train_problem_features = problem_feature_mat[train_problems, :]
+                    kmeans_model = sklearn.cluster.KMeans(n_clusters=20, n_init='auto', random_state=0).fit(train_problem_features)
+                    problem_labels = kmeans_model.predict(problem_feature_mat) # predict labels for all problems
+                    rand_index = sklearn.metrics.rand_score(ref_assignment, problem_labels)
+                    ri.append(rand_index)
+            else:
+                continue 
+                
+            ix = (all_results_df['model'] == model) & (all_results_df['dataset'] == dataset_name)
+            ri_col[ix] = ri
+            
+    all_results_df['raw_rand_index'] = ri_col
+
+    all_results_df.to_csv(results_file, index=False)
+    print(all_results_df)
+
+def get_problem_skill_assignment(df):
+
+    problems_to_skills = dict(zip(df['problem'], df['skill']))
+    n_problems = np.max(df['problem']) + 1
+    return np.array([problems_to_skills[p] for p in range(n_problems)])
+
+
+if __name__ == "__main__":
+    main()
