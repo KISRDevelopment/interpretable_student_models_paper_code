6116526615be14a3e1175f98b12b30817249f49a
Mohammad Khajah <mmkhajah@gmail.com>
2024-05-07 11:00:35 +0000

    add recovered % as clustering metric

--- cluster_metrics.py
+++ cluster_metrics.py
@@ -1,6 +1,49 @@
 import numpy as np 
 import sklearn.metrics
 
+def recovered(ref_clustering, pred_clustering, thres=0.9):
+
+    # ref_M = make_membership_profile(ref_clustering) # C_ref x P
+    # pred_M = make_membership_profile(pred_clustering) # C_pred x P
+
+    r_clusters = np.unique(ref_clustering)
+    p_clusters = np.unique(pred_clustering)
+
+
+    R = np.zeros((r_clusters.shape[0], p_clusters.shape[0]))
+    
+    print("Processing ", R.shape)
+    counts = np.zeros(r_clusters.shape[0])
+    for i in range(R.shape[0]):
+        u = ref_clustering == r_clusters[i]
+        counts[i] = np.sum(u)
+        for j in range(R.shape[1]):
+            dst = fast_bacc(u, pred_clustering == p_clusters[j])
+            R[i, j] = dst 
+    print("Done ", R.shape)
+    
+    R = np.sum(counts * (np.max(R, axis=1) >= thres)) / ref_clustering.shape[0]
+    
+    return R 
+
+def fast_bacc(ytrue, ypred):
+
+    recall00 = np.sum((ytrue == 0) & (ypred == 0)) / np.sum(ytrue == 0)
+    recall11 = np.sum((ytrue == 1) & (ypred == 1)) / np.sum(ytrue == 1)
+
+    return (recall00 + recall11) / 2
+
+def make_membership_profile(clustering):
+
+    r = np.unique(clustering)
+    
+    M = np.zeros((r.shape[0], len(clustering)))
+    for i in range(M.shape[0]):
+        M[i, :] = clustering == r[i]
+    
+
+    return M 
+
 def make_contingency_table(clustering_a, clustering_b):
 
     r = np.unique(clustering_a)
@@ -41,12 +84,24 @@ def _fmeasure(C):
 
     return f
 
-
 def main():
     C = make_contingency_table([0, 0, 1, 2, 3, 3], [1, 1, 2, 0, 5, 10])
     print(C)
-    f = fmeasure(C)
+    f = fmeasure([0, 0, 1, 2, 3, 3], [1, 1, 2, 0, 5, 10])
     print(f)
-   
+    
+    R = recovered(np.array([0, 0, 1, 2, 3, 3]), np.array([1, 1, 2, 0, 5, 10]), thres=0.9)
+    print(R)
+
+    a, b = np.array([0, 0, 1, 1, 0, 0, 0, 0, 1]), np.array([0, 1, 0,  0, 0, 0, 0, 0, 1])
+    x = sklearn.metrics.balanced_accuracy_score(a, b)
+    y = fast_bacc(a, b)
+
+    print(x, y)
+
+    x = sklearn.metrics.balanced_accuracy_score(a, np.zeros_like(a))
+    y = fast_bacc(a, np.zeros_like(a))
+
+    print(x, y)
 if __name__ == "__main__":
     main()
--- posthoc_rand_index.py
+++ posthoc_rand_index.py
@@ -15,6 +15,7 @@ def main():
     models = set(all_results_df['model'])
     ri_col = np.zeros(all_results_df.shape[0])
     fi_col = np.zeros_like(ri_col)
+    vi_col = np.zeros_like(fi_col)
     for dataset_name in datasets:
         
         df = pd.read_csv("data/datasets/%s.csv" % dataset_name)
@@ -23,6 +24,12 @@ def main():
         problem_feature_mat = np.load("data/datasets/%s.embeddings.npy" % dataset_name.replace('_blocked', '').replace('_interleaved',''))
         
         for model in models:
+
+            ri = []
+            fi = []
+            vi = []
+
+            pred_assignments = []
             if model.startswith('sd'):
                 if 'realistic' in dataset_name:
 
@@ -32,21 +39,14 @@ def main():
                 params = np.load(params_path)
                 Aprior = params['Aprior'] # Splits x Problems x KCs
             
-                ri = []
-                fi = []
                 for i in range(Aprior.shape[0]):
                     Q = Aprior[i, :, :] 
                     pred_assignment = np.argmax(Q, axis=1)
-                    rand_index = sklearn.metrics.rand_score(ref_assignment, pred_assignment)
-                    fmeasure = cluster_metrics.fmeasure(ref_assignment, pred_assignment)
-                    ri.append(rand_index)
-                    fi.append(fmeasure)
-                
+                    pred_assignments.append(pred_assignment)
+
             elif model.startswith('clustering') and not dataset_name.startswith('sd_1_'):
                 splits = np.load("data/splits/%s.npy" % dataset_name)
                 
-                ri = []
-                fi = []
                 for s in range(splits.shape[0]):
                     split = splits[s, :]
 
@@ -60,19 +60,25 @@ def main():
                     train_problem_features = problem_feature_mat[train_problems, :]
                     kmeans_model = sklearn.cluster.KMeans(n_clusters=20, n_init='auto', random_state=0).fit(train_problem_features)
                     problem_labels = kmeans_model.predict(problem_feature_mat) # predict labels for all problems
-                    rand_index = sklearn.metrics.rand_score(ref_assignment, problem_labels)
-                    ri.append(rand_index)
-                    fi.append(cluster_metrics.fmeasure(ref_assignment, problem_labels))
+                    pred_assignments.append(problem_labels)
+                
             else:
                 continue 
-                
+            
+            for pred_assignment in pred_assignments:
+                ri.append(sklearn.metrics.rand_score(ref_assignment, pred_assignment))
+                #fi.append(cluster_metrics.fmeasure(ref_assignment, pred_assignment))
+                vi.append(cluster_metrics.recovered(ref_assignment, pred_assignment))
+            
             ix = (all_results_df['model'] == model) & (all_results_df['dataset'] == dataset_name)
             ri_col[ix] = ri
-            fi_col[ix] = fi 
+            #fi_col[ix] = fi 
+            vi_col[ix] = vi 
 
     all_results_df['raw_rand_index'] = ri_col
-    all_results_df['fmeasure'] = fi_col
-    
+    #all_results_df['fmeasure'] = fi_col
+    all_results_df['recovered'] = vi_col
+
     all_results_df.to_csv(results_file, index=False)
     print(all_results_df)
 
